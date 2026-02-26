edbc354232117624918ade09a4d6aa46f96f0ac6
trosenow27 <t.rosenow@tcu.edu>
2026-02-26 13:38:53 -0600

    Refactor extract_features to use new stock tickers
    Updated stock tickers in extract_features function and modified the way features are extracted.

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -20,26 +20,31 @@ def extract_features():
     
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['MSFT', 'IBM', 'GOOGL']
-    ccy_tickers = ['DEXJPUS', 'DEXUSUK']
-    idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
+    stk_tickers = ['MPWR', 'AAPL'] #['MSFT', 'IBM', 'GOOGL']
+    #ccy_tickers = ['DEXJPUS', 'DEXUSUK']
+    #idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
     #stk_data = web.DataReader(stk_tickers, 'yahoo')
     ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
     idx_data = web.DataReader(idx_tickers, 'fred', start=START_DATE, end=END_DATE)
 
-    Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
-    Y.name = Y.name[-1]+'_Future'
-    
-    X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
-    X1.columns = X1.columns.droplevel()
-    X2 = np.log(ccy_data).diff(return_period)
-    X3 = np.log(idx_data).diff(return_period)
+    #Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
+    Y = np.log(stk_data.loc[:, ('Adj Close', 'COST')]).diff(return_period).shift(-return_period)
+    Y = stk_data.loc[:, ('Adj Close', 'AAPL')]
+    Y.name = 'AAPL'
 
-    X = pd.concat([X1, X2, X3], axis=1)
+    X = stk_data.loc[:, ('Adj Close', 'MPWR')]
+    X.name = 'MPWR
     
-    dataset = pd.concat([Y, X], axis=1).dropna().iloc[::return_period, :]
+    #X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+    #X1.columns = X1.columns.droplevel()
+    #X2 = np.log(ccy_data).diff(return_period)
+    #X3 = np.log(idx_data).diff(return_period)
+
+    #X = pd.concat([X1, X2, X3], axis=1)
+    
+    dataset = pd.concat([Y, X], axis=1).dropna().#iloc[::return_period, :]
     Y = dataset.loc[:, Y.name]
     X = dataset.loc[:, X.columns]
     dataset.index.name = 'Date'
@@ -66,4 +71,5 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
