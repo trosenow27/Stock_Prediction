b3a79fc256310d024fff1216516bda3df19078b3
trosenow27 <t.rosenow@tcu.edu>
2026-03-26 12:49:26 -0500

    Replace AAPL with MCD and pairs

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -60,8 +60,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["MPWR", "AAPL"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
+        "keys": ["AON", "MCD"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["AON", "MCD"]]
 }
 
 def load_pipeline(_session, bucket, key):
