7562631b5d29400ef844e703cf2361f6bb855d01
trosenow27 <t.rosenow@tcu.edu>
2026-03-26 12:47:54 -0500

    Add files via upload

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -18,9 +18,8 @@ from sagemaker.serializers import NumpySerializer
 from sagemaker.deserializers import NumpyDeserializer
 
 from imblearn.pipeline import Pipeline
-
 import shap
-
+import importlib
 
 # Setup & Path Configuration
 warnings.simplefilter("ignore")
@@ -28,18 +27,18 @@ warnings.simplefilter("ignore")
 # Fix path for Streamlit Cloud (ensure 'src' is findable)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
+
 if project_root not in sys.path:
     sys.path.append(project_root)
 
-from src.feature_utils import get_bitcoin_historical_prices
+from src.feature_utils import extract_features_pair
 
 # Access the secrets
 aws_id = st.secrets["aws_credentials"]["AWS_ACCESS_KEY_ID"]
 aws_secret = st.secrets["aws_credentials"]["AWS_SECRET_ACCESS_KEY"]
 aws_token = st.secrets["aws_credentials"]["AWS_SESSION_TOKEN"]
 aws_bucket = st.secrets["aws_credentials"]["AWS_BUCKET"]
-aws_endpoint_bitcoin = st.secrets["aws_credentials"]["AWS_ENDPOINT"]
-
+aws_endpoint = st.secrets["aws_credentials"]["AWS_ENDPOINT"]
 
 # AWS Session Management
 @st.cache_resource # Use this to avoid downloading the file every time the page refreshes
@@ -51,25 +50,18 @@ def get_session(aws_id, aws_secret, aws_token):
         region_name='us-east-1'
     )
 
-
 session = get_session(aws_id, aws_secret, aws_token)
-
 sm_session = sagemaker.Session(boto_session=session)
 
 # Data & Model Configuration
-df_prices = get_bitcoin_historical_prices()
-
-# Dynamic bounds for Bitcoin model
-MIN_VAL = 0.5 * df_prices.iloc[:, 0].min()
-MAX_VAL = 2.0 * df_prices.iloc[:, 0].max()
-DEFAULT_VAL = df_prices.iloc[:, 0].mean()
+df_features = extract_features_pair()
 
 MODEL_INFO = {
         "endpoint": aws_endpoint,
-        "explainer": 'explainer_bitcoin.shap',
-        "pipeline": 'finalized_model.tar.gz',
-        "keys": ["YUM","MCD"],
-        "inputs": [{"name": k``, "type": "number", "min": 0, "default": DEFAULT_VAL, "step": 100.0}]
+        "explainer": 'explainer_pair.shap',
+        "pipeline": 'finalized_pair_model.tar.gz',
+        "keys": ["MPWR", "AAPL"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -101,19 +93,20 @@ def load_shap_explainer(_session, bucket, key, local_path):
 
 # Prediction Logic
 def call_model_api(input_df):
-    
+
     predictor = Predictor(
         endpoint_name=MODEL_INFO["endpoint"],
         sagemaker_session=sm_session,
         serializer=NumpySerializer(),
         deserializer=NumpyDeserializer() 
     )
-    
+
     try:
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
         mapping = {-1: "SELL", 0: "HOLD", 1: "BUY"}
         return mapping.get(pred_val, pred_val), 200
+        #return round(float(pred_val), 4), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
 
@@ -125,29 +118,21 @@ def display_explanation(input_df, session, aws_bucket):
     full_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
     preprocessing_pipeline = Pipeline(steps=full_pipeline.steps[:-2])
     input_df_transformed = preprocessing_pipeline.transform(input_df)
-    shap_values = explainer(input_df_transformed)
     feature_names = full_pipeline[1:4].get_feature_names_out()
-
-    exp = shap.Explanation(
-        values=shap_values[0, :, 0],       # The matrix of SHAP values
-        base_values=explainer.expected_value[0], # The intercept/base value
-        data=input_df_transformed[0],        # The actual feature values for that user
-        feature_names=feature_names        # Your list of names
-        )
+    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
+    shap_values = explainer(input_df_transformed)
 
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
-    shap.plots.waterfall(exp)
+    shap.plots.waterfall(shap_values[0, :, 0])
     st.pyplot(fig)
     # top feature   
-    top_feature = pd.Series(exp.values, index=exp.feature_names).abs().idxmax()
+    top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
     st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
 
-
 # Streamlit UI
-st.set_page_config(page_title="ML Deployment Compiler", layout="wide")
-st.title("👨‍💻 ML Deployment Compiler")
-
+st.set_page_config(page_title="ML Deployment", layout="wide")
+st.title("👨‍💻 ML Deployment")
 
 with st.form("pred_form"):
     st.subheader(f"Inputs")
@@ -158,7 +143,7 @@ with st.form("pred_form"):
         with cols[i % 2]:
             user_inputs[inp['name']] = st.number_input(
                 inp['name'].replace('_', ' ').upper(),
-                min_value=inp['min'], max_value=inp['max'], value=inp['default'], step=inp['step']
+                min_value=inp['min'], value=inp['default'], step=inp['step']
             )
     
     submitted = st.form_submit_button("Run Prediction")
@@ -166,8 +151,8 @@ with st.form("pred_form"):
 if submitted:
 
     data_row = [user_inputs[k] for k in MODEL_INFO["keys"]]
-    # Prepare data (Stock predictor uses df_features, Bitcoin uses df_prices)
-    base_df = df_prices
+    # Prepare data
+    base_df = df_features
     input_df = pd.concat([base_df, pd.DataFrame([data_row], columns=base_df.columns)])
     
     res, status = call_model_api(input_df)
